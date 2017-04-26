ab8a383532e8933bff1a7647415fa47a1acf0591
visitor <visitor@DESKTOP-P473OEV>
2017-04-26 13:29:09 +0900

    We are the world

---
+++ JavaPractice/src/inheritance/Chinese.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package inheritance;
+
+/**
+ * <pre>
+ * inheritance
+ *    |_ Chinese
+ * 
+ * 1. 개요 : 
+ * 2. 작성일 : 2017. 4. 26.
+ * </pre>
+ * 
+ * @author visitor
+ * @version : 1.0
+ */
+public class Chinese extends Person{
+	public Chinese(String name){
+		super(name);
+	}
+	
+	public void sayHello(){
+		System.out.println("니하오");
+		
+	}
+	public void sayBye(){
+		System.out.println("짜이찌엔");
+	}
+
+	
+	public String toString() {
+		return "저는 중국사람" + super.getName() + "입니다.";
+	}
+}
